d5506d23cff2eac2cb08e5708185b18037147118
00010216 <00010216@uca.edu.sv>
2019-06-22 12:02:06 -0600

    Base project with MVC and base configurations

---
+++ tarea2_00010216/src/com/uca/capas/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.uca.capas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public ModelAndView initMain() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("main");
+		mav.addObject("message","Hi");
+		return mav;
+	}
+}
